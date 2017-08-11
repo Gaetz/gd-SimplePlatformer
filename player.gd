0be6f8b5efb7dbd1b19f8deeb32935d87c8eab7f
Gaetz <gaetz@rufflerim.com>
2017-08-11 21:06:57 +0200

    Refactoring input for something more godot-esque

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 # -1 for going left, 1 for going right
 var input_direction = 0
-var direction = 0
+var direction = 1
 
 var speed = 0
 var ACCELERATION = 1000
@@ -13,23 +13,26 @@ const MAX_SPEED = 600
 
 func _ready():
 	set_process(true)
+	set_process_input(true)
+
+func _input(event):
+	var is_moving_left = event.is_action_pressed("move_left")
+	var is_moving_right = event.is_action_pressed("move_right")
+	# Input direction choice
+	if is_moving_left:
+		input_direction = -1
+	elif is_moving_right:
+		input_direction = 1
+	elif not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		input_direction = 0
+	# Speed reducing when changing direction
+	if is_moving_left or is_moving_right and input_direction:
+		if direction == -input_direction:
+			speed /= 8
+			direction = input_direction
 
 func _process(delta):
-	# Remember last direction
-	if input_direction:
-		direction = input_direction
-	
-	# Input direction choice
-	if Input.is_action_pressed("ui_left"):
-		input_direction = -1
-	elif Input.is_action_pressed("ui_right"):
-		input_direction = 1
-	else:
-		input_direction = 0
-	
 	# Variation
-	if(direction == -input_direction):
-		speed /= 8
 	if input_direction:
 		speed += ACCELERATION * delta
 	else:
