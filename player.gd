71d255800f7b519c28ca2b443d56b1e760ff5cdc
Gaetz <gaetz@rufflerim.com>
2017-08-11 23:42:04 +0200

    Double jump limit

--- player.gd
+++ player.gd
@@ -7,12 +7,15 @@ var direction = 1
 var speed_x = 0
 var speed_y = 0
 var velocity = Vector2()
+var jump_count = 0
 
-const MAX_SPEED_X = 600
+const MAX_SPEED_X = 700
 const JUMP_FORCE = 800
 const GRAVITY = 2000
-const ACCELERATION = 1000
-const DECELERATION = 2000
+const ACCELERATION = 2000
+const DECELERATION = 5000
+const MAX_JUMP_COUNT = 2
+const MAX_FALL_SPEED = 1400
 
 func _ready():
 	set_process(true)
@@ -20,7 +23,9 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("jump"):
-		speed_y = - JUMP_FORCE
+		if jump_count <= MAX_JUMP_COUNT - 1:
+			speed_y = - JUMP_FORCE
+			jump_count += 1
 	
 
 func _process(delta):
@@ -48,6 +53,8 @@ func _process(delta):
 	speed_x = clamp(speed_x, 0, MAX_SPEED_X)
 	# Gravity
 	speed_y += GRAVITY * delta
+	if speed_y > MAX_FALL_SPEED:
+		speed_y = MAX_FALL_SPEED
 	# Move
 	velocity.x = speed_x * delta * direction
 	velocity.y = speed_y * delta
@@ -60,5 +67,8 @@ func _process(delta):
 		var final_movement = normal.slide(movement_remainder)
 		# Manage vertical movement
 		speed_y = normal.slide(Vector2(0, speed_y)).y
+		# Reset jump count if colliding floor
+		if normal == Vector2(0, -1):
+			jump_count = 0
 		# Execute movement
 		move(final_movement)
